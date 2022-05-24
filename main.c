000feeecdff689c4d159d3907ad017f267df8465
Tisin-Gleb <judaskurwa@gmail.com>
2022-05-25 03:22:14 +0700

    testus

--- main.c
+++ main.c
@@ -1,11 +1,99 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// int Suffix(FILE *input, char *text, char *substring)
+// {
+//     int text_length = FileLength(input);
+//     int substring_length = strlen(substring);
+//     int i = 0;
+//     int j = 0;
+
+//     for (i = 0; i < text_length; i++)
+//     {
+//         if (text[i] == substring[j]){
+//             j++;
+//             continue;
+//         }else{
+//             j = 0;
+//         }
+//         if (j == substring_length) return i;
+//     }
+
+//     puts("Have no prefix");
+//     return 0;
+// }
+
+// int Suffix(FILE *input, char *text, char *substring)
+// {
+//     if (feof(input)) return -1;
+
+//     int text_length = FileLength(input);
+//     int substring_length = strlen(substring);
+
+//     int i = ftell(input);
+//     int suffix = 0;
+
+//     while (i < text_length)
+//     {
+//         i = ftell(input);
+//         if (text[i] == substring[suffix]){
+//             ++suffix;
+//             if (suffix == substring_length) return suffix;
+//             continue;
+//         }else{
+//             return suffix;
+//         }
+//         fseek(input, 1, SEEK_CUR);
+//     }
+    
+//     //if (getchar(input) == EOF && suffix != 0) return -1; dont uncommment
+
+//     return suffix;
+// }
+
 typedef struct {
-    unsigned char current_state;
-	signed char read_char; 
-	unsigned char next_state;
+    int current_state;
+	char read_char; 
+	int next_state;
 } State;
+
+//Finite-state machine
+int FSM(FILE *input, char *sample)
+{
+    char *text;
+    text = FileTextInArray(input);
+
+    int file_length = FileLength(input);
+
+    State state;
+    state.current_state = 0;
+
+
+
+    int i = 0;
+    int j = 0;
+    for (; i < file_length; i++)
+    {
+        if (text[i] == sample[j]){
+            j++;
+            state.current_state = 1;
+            StateTransition(state.current_state, text, i);    
+        }        
+    }
+}
+
+int StateTransition(int current_state, char *text, int current_symbol)
+{
+    int j = 1;
+    for (; i < count; i++)
+    {
+        
+    }
+    
+}
 
 int FileLength(FILE *input)
 {
@@ -15,10 +103,10 @@ int FileLength(FILE *input)
     return file_length;
 }
 
-char* FileTextInArray(FILE *input, char *file_text)
+char* FileTextInArray(FILE *input)
 {
     int file_length = FileLength(input);
-    file_text = malloc(file_length);
+    char *file_text = malloc(file_length);
     if (file_text == NULL) exit(1);
 
     int i = 0;
@@ -36,20 +124,44 @@ void FileFree(char *file_text)
     free(file_text);
 }
 
+void generate_file()
+{
+    FILE *file;
+    file = fopen("text.le", "w");
+    int number;
+    char c;
+    for (int i = 0; i < 1000000; i++)
+    {
+        number = rand() % 2;
+        c = number + 97;
+        fprintf(file, "%c", c);
+    }
+    fclose(file);
+}
+
 int main()
 {
-    FILE *input;
+    srand(time(NULL));
+    generate_file();
+    FILE *input, *suf_test;
+    char subsrting[] = "aaabab";
     if ( (input = fopen("input.le", "r")) == NULL) return 0;
     printf("Введите строку для поиска: ");
 
+    if ( (suf_test = fopen("input.le", "r")) == NULL) return 0;
+
     char *file_text = NULL;
     int file_length = FileLength(input);
-    file_text = FileTextInArray(input, file_text);
-    for (int i = 0; i < file_length; i++)
-    {
-        printf("%c", file_text[i]);
-    }
+     = FileTextInArray(input);
     
+    // ПРОТЕСТИРУЙ ФУНКЦИЮ СУФФИКС
+    // АВТОМАТЫ ЧЕРЕЗ СВИТЧ КАЙС
+    // ШАБЛОНЫ: ПЕРЕХОД ЕСЛИ ЕСТЬ СИМВОЛ ЕСТЬ В МАССИВЕ ШАБЛОНА
+    
+    while(1){
+        Suffix(suf_test, );
+    }
+
     FileFree(file_text);
     fclose(input);
     puts("Okay");
